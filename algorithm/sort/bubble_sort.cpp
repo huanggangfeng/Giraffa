07e110ccaad7e6963ef1bf835d87d16c2365374f
Andrew <andrew@andrew-linux.apac.corp.natinst.com>
2015-02-13 13:55:32 +0800

    bubble sort

--- algorithm/sort/bubble_sort.cpp
+++ algorithm/sort/bubble_sort.cpp
@@ -10,13 +10,26 @@ int main()
 {
    vector<int> a(10);
    vector<int>::iterator it;
+   int temp;
 
    srand(time(NULL));
    for (it = a.begin(); it!=a.end();it++)
       *it = rand()%100;
    
-   sort(a.begin(), a.end());
-   
+//   sort(a.begin(), a.end());
+   for (int i = 0; i < a.size(); ++i)
+   {  
+      for (int j = 0; j < a.size() - i - 1; ++j)
+      {
+         if (a[j] > a[j+1])
+         {
+            temp = a[j+1];
+            a[j+1] = a[j];
+            a[j] = temp;
+         }
+      }
+   }   
+
    for (it = a.begin(); it!=a.end();it++)
       cout << *it << " ";
    cout << endl;
