a72c755228c178d54605f29fe41823c0d1b26b3e
Gangfeng Huang <huanggangfeng@gmail.com>
2017-07-28 16:49:48 +0800

    fixup! Add some test code about c++

--- stl/test_remove_erase.cpp
+++ stl/test_remove_erase.cpp
@@ -5,26 +5,23 @@
 void print_vector(std::vector<int> &v)
 {
    for(auto &i: v)
-   {
       std::cout << i << ' ';
-   }
    std::cout << '\n';
 }
 
 int main(int argc, char const *argv[]) {
    std::vector<int> v;
    v.assign(10, 0);
-   print_vector(v);
    for (size_t i = 0; i < v.size(); i++)
-   {
       v[i] = i;
-   }
+   std::cout << "Init vector" << '\n';
    print_vector(v);
    auto it = std::remove_if(v.begin(), v.end(),
-                            [](int value) {return (value%2 ==0);});
-
+                           [](int value) {return (value%2 ==0);});
+   std::cout << "After remove_if" << '\n';
    print_vector(v);
    v.erase(it, v.end());
+   std::cout << "After erase" << '\n';
    print_vector(v);
    return 0;
 }
